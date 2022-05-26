bbc93d44f0850d4bad4af0578adcb9f7af5248c6
JoseLobos <88002545+JoseLobos@users.noreply.github.com>
2022-05-25 19:00:58 -0600

    fin

--- app/src/main/java/com/example/holamundokotlin/MainActivity.kt
+++ app/src/main/java/com/example/holamundokotlin/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.holamundokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import  kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvHolaMundo.text = "¡Bienvenidos al curso de Android con Kotlin";
+        btEjecutar.setOnClickListener{
+            // Ejemplo de asiganción de valor a una vista
+            tvCadena.text = "Tu nombre es: Juan Perez"
+
+            Toast.makeText(this,"Has cambiado el texto",Toast.LENGTH_LONG).show()
+        }
     }
 }
--- app/src/main/java/com/example/holamundokotlin/MainActivity2.kt
+++ app/src/main/java/com/example/holamundokotlin/MainActivity2.kt
@@ -5,34 +5,58 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main2.*
+
 
 class MainActivity2 : AppCompatActivity(), TextWatcher {
 
-    // crear objetos de tipo EditText para enlace con las vistas
+
     private var eNombre: EditText? = null
     private var eEdad: EditText? = null
-    // crear variables para recibir los datos y procesarlos
+    private var textNombre: TextView? = null
+    private var textEdad: TextView? = null
     private var nombre: String? = null
-    private var etEdad = 0
+    private var edad = 0
+    private var miEdad: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
         eNombre = findViewById(R.id.etNombre) as EditText
         eEdad = findViewById(R.id.etEdad) as EditText
-        // Asignar los valores de la vista al objeto de esta clase.
+        textNombre = findViewById(R.id.tvMostrarNombre) as TextView
+        textEdad = findViewById(R.id.etEdad) as TextView
         eNombre?.addTextChangedListener(this)
+        eEdad!!.addTextChangedListener(this)
+        btEjecutarEstado.setOnClickListener(){
+            nombre = eNombre?.text.toString()
+            edad = Integer.valueOf(eEdad?.text.toString())
+            if(edad < 18)
+                tvDatosIngresados.text = "Eres menor de edad"
+            else
+                tvDatosIngresados.text = "Eres mayor de edad"
+            //Toast.makeText(this,nombre,Toast.LENGTH_LONG).show()
+        }
     }
 
     override fun afterTextChanged(p0: Editable?) {
-        TODO("Not yet implemented")
+
     }
     override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-        TODO("Not yet implemented")
+
     }
-    // Esta función se ejecutan en el momento de ingresar un dato
+
     override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-        Toast.makeText(this,p0.toString(), Toast.LENGTH_LONG).show()
+        nombre = eNombre?.text.toString()
+
+        miEdad = eEdad?.text.toString()
+        if(nombre !="")tvMostrarNombre?.text = nombre
+        if(miEdad !="")tvMostrarEdad?.text = miEdad
+        //textEdad?.text = edad
+        //Toast.makeText(this,p0.toString(),Toast.LENGTH_LONG).show()
     }
 }
